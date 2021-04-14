d75568d57128bbfb3419010403332ba6260bd019
Aaron Toney <aaron.p.toney@gmail.com>
2021-04-14 05:59:21 +0100

    Move rf_display into rf_display_ssd1306 in the rf_common_ws

---
+++ src/rf_power_monitor/nodes/rf_power_monitor_tester_node.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+""" 
+    Node listening to messages for the power monitoring node in order 
+    to test it. Could just also just launch it and echo the ros topic. 
+
+    This node is purely a testing tool. 
+"""
+
+import rospy
+from rf_power_monitor.msg import RFPowerMonitorData
+
+def pm_callback(data):
+    rospy.loginfo("voltage: %f, current: %f, power: %f\r\n" % (data.battery_voltage, data.current, data.power))
+
+def listen_for_power_messages():
+    rospy.init_node('rf_power_monitor', anonymous=True)
+    rospy.Subscriber("rf_power_monitor_topic", RFPowerMonitorData, pm_callback)
+    rospy.spin()
+    
+if __name__ == '__main__':
+    try:
+        listen_for_power_messages()
+    except rospy.ROSInterruptException:
+        pass
+    
+
+
+
+    
